d96ac452d009c76135e0542cad1b17f6492f5a1b
François Beaufort <beaufort.francois@gmail.com>
2014-01-14 11:08:01 +0100

    Split app.js into app.js and save.js

--- app.js
+++ app.js
@@ -223,41 +223,6 @@ function drawCode(showTransition) {
   }, (drawCodeTimeoutID === null) ? 0 : 200); // Must be kept in sync with CSS Transition.
 }
 
-function dataURItoBlob(dataURI) {
-  // convert base64 to raw binary data held in a string
-  // doesn't handle URLEncoded DataURIs
-  var byteString = atob(dataURI.split(',')[1]);
-
-  // separate out the mime component
-  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
-
-  // write the bytes of the string to an ArrayBuffer
-  var ab = new ArrayBuffer(byteString.length);
-  var ia = new Uint8Array(ab);
-  for (var i = 0; i < byteString.length; i++) {
-    ia[i] = byteString.charCodeAt(i);
-  }
-
-  // write the ArrayBuffer to a blob, and you're done
-  return new Blob([ab], {
-    "type": mimeString
-  });
-};
-
-function exportCode() {
-  var config = {
-    type: 'saveFile',
-    suggestedName: 'marmoset-' + Date.now() + '.png'
-  };
-  chrome.fileSystem.chooseEntry(config, function (writableEntry) {
-    var blob = dataURItoBlob(renderer.domElement.toDataURL('png'));
-    writableEntry.createWriter(function (writer) {
-      writer.write(blob);
-    });
-  });
-}
-document.querySelector('#exportButton').addEventListener('click', exportCode);
-
 
 var themeSelector = document.querySelector('#themeSelector');
 
